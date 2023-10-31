9acff9d3449fd189677805c3ea34ceaae4e004f0
Max Dillon <maxdillon13124@gmail.com>
2023-10-31 11:40:55 -0400

    feat: add prettier and eslint, demonstrate dexie where statement

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
-import { db } from "../db";
-import { useLiveQuery } from "dexie-react-hooks";
+import { useState } from 'react'
+import { db } from '../db'
+import { useLiveQuery } from 'dexie-react-hooks'
 
 async function addFriend(name: string, age: number) {
   try {
-    const id = await db.friends.add({ name, age });
-    console.log(id);
+    const id = await db.friends.add({ name, age })
+    console.log(id)
   } catch (e) {
-    console.log(`Failed to add ${name}: ${e}`);
+    console.log(`Failed to add ${name}: ${e}`)
   }
 }
 
 function Friends() {
-  const friends = useLiveQuery(async () => db.friends.toArray());
+  const friends = useLiveQuery(async () =>
+    db.friends.where('age').above(5).toArray(),
+  )
   return (
     <table>
       <thead>
@@ -22,7 +24,7 @@ function Friends() {
         </tr>
       </thead>
       <tbody>
-        {friends?.map((friend) => (
+        {friends?.map(friend => (
           <tr>
             <td>{friend.name}</td>
             <td>{friend.age}</td>
@@ -30,30 +32,30 @@ function Friends() {
         ))}
       </tbody>
     </table>
-  );
+  )
 }
 
 function Admin() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState(0);
+  const [name, setName] = useState('')
+  const [age, setAge] = useState(0)
 
   return (
     <>
-      <h1 className="text-2xl">Phil 208 Ethics Project</h1>
+      <h1 className='text-2xl'>Phil 208 Ethics Project</h1>
       <p>Here is some text!!!</p>
       <Friends />
       <input
-        className="border-2"
-        onChange={(e) => setName(e.target.value)}
+        className='border-2'
+        onChange={e => setName(e.target.value)}
       ></input>
       <input
-        className="border-2"
-        type="number"
-        onChange={(e) => setAge(parseInt(e.target.value))}
+        className='border-2'
+        type='number'
+        onChange={e => setAge(parseInt(e.target.value))}
       ></input>
       <button onClick={() => addFriend(name, age)}>Submit</button>
     </>
-  );
+  )
 }
 
-export default Admin;
+export default Admin
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: './',
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: 'jsdom',
   },
-});
+})
