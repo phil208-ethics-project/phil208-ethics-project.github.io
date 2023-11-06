15bd645bfb2600b8b5503e2a300d94e5156edb5b
Max Dillon <maxdillon13124@gmail.com>
2023-11-06 14:17:37 -0500

    feat: create typescript database schema

--- src/db.ts
+++ src/db.ts
@@ -1,23 +1,77 @@
 // db.ts
 import Dexie, { Table } from 'dexie'
 
-export interface Friend {
+export interface Student {
   id?: number
-  name: string
+  first_name: string
+  last_name: string
+  gender: 'male' | 'female' | 'nonbinary' | 'n/a'
   age: number
 }
 
-export class MySubClassedDexie extends Dexie {
+export interface FictionalGrade {
+  id?: number
+  student_id: number
+  v: boolean
+  kd: boolean
+  ca: boolean
+  i: boolean
+  e: boolean
+  l: boolean
+  go: boolean
+  mi: boolean
+}
+
+export interface InformationalGrade {
+  id?: number
+  student_id: number
+  v: boolean
+  kd: boolean
+  ca: boolean
+  i: boolean
+  e: boolean
+  l: boolean
+  go: boolean
+  mi: boolean
+}
+
+export interface SpellingGrade {
+  id?: number
+  student_id: number
+  phonetic_short_vowels: string
+  phonetic_consonant_blends: string
+  phonetic_consonant_digraphs: string
+  transitional_long_vowels: string
+  transitional_complex_vowels: string
+  fluent_inflectional_endings: string
+  fluent_multisyllabic_words_2_syllabes: string
+  advanced_multisyllabic_words_3_syllabes: string
+}
+
+export interface ReadingLevelGrade {
+  student_id: number
+  reading_level: number
+}
+
+export class StudentGradesDB extends Dexie {
   // 'friends' is added by dexie when declaring the stores()
   // We just tell the typing system this is the case
-  friends!: Table<Friend>
+  students!: Table<Student, number>
+  fictional_grades!: Table<FictionalGrade, number>
+  informational_grades!: Table<InformationalGrade, number>
+  spelling_grades!: Table<InformationalGrade, number>
+  reading_grades!: Table<InformationalGrade, number>
 
   constructor() {
-    super('myDatabase')
+    super('StudentGrades')
     this.version(1).stores({
-      friends: '++id, name, age', // Primary key and indexed props
+      students: '++id,[first_name+last_name]',
+      fictional_grades: '++id, student_id',
+      informational_grades: '++id, student_id',
+      spelling_grades: '++id, student_id',
+      reading_grades: '++id, student_id',
     })
   }
 }
 
-export const db = new MySubClassedDexie()
+export const db = new StudentGradesDB()
