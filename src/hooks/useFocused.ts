326f16189b685e8a557e57e940d07ee90739890b
Max Dillon <maxdillon13124@gmail.com>
2023-11-12 15:58:51 -0500

    fix: improve caching of useFocused hook

--- src/hooks/useFocused.ts
+++ src/hooks/useFocused.ts
@@ -1,15 +1,10 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 export default function useFocused() {
   const [isFocused, setIsFocused] = useState(false)
+  const onBlur = useCallback(() => setIsFocused(false), [setIsFocused])
+  const onFocus = useCallback(() => setIsFocused(true), [setIsFocused])
 
-  const onBlur = () => {
-    setIsFocused(false)
-  }
-
-  const onFocus = () => {
-    setIsFocused(true)
-  }
   const props = {
     onBlur,
     onFocus,
