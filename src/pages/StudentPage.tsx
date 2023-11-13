02c8111ea7fce00163a2f0b2c469eb0cad85a974
Max Dillon <maxdillon13124@gmail.com>
2023-11-12 22:10:14 -0500

    fix: switch from location to param for student page data update

--- src/pages/StudentPage.tsx
+++ src/pages/StudentPage.tsx
@@ -1,19 +1,18 @@
 import { db } from '@db'
 
 import { useLiveQuery } from 'dexie-react-hooks'
-import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export default function StudentPage() {
   const { id } = useParams()
   const navigate = useNavigate()
-  const location = useLocation()
 
   const student = useLiveQuery(async () => {
     return db.students
       .get(parseInt(id || ''))
       .then(student => student ?? navigate('/'))
       .catch(() => navigate('/'))
-  }, [location])
+  }, [id])
 
   return (
     <>
