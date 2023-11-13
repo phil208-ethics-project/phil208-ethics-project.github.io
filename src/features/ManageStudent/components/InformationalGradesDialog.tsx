143439a5f0967bdea43c7368fc325b2deeaa1fc3
Max Dillon <maxdillon13124@gmail.com>
2023-11-12 23:24:56 -0500

    feat: exit dialog with click outside
    enable using useOnClickOutside and useDelayedValue hooks
    useDelayedValue important to prevent from closing immediately

--- src/features/ManageStudent/components/InformationalGradesDialog.tsx
+++ src/features/ManageStudent/components/InformationalGradesDialog.tsx
@@ -1,15 +1,23 @@
+import useDelayedValue from '@hooks/useDelayedValue'
+import useOnClickOutside from '@hooks/useOnClickOutside'
+
 interface Props {
   isOpen: boolean
   exit: () => void
 }
 
 export default function InformationalGradesDialog({ isOpen, exit }: Props) {
+  const open = useDelayedValue(isOpen)
+  const ref = useOnClickOutside<HTMLDivElement>(
+    () => open && exit(),
+    [isOpen, open],
+  )
   return (
     <div
       data-isopen={isOpen}
       className='absolute w-full h-full bg-opacity-70 bg-black top-0 right-0 p-28 data-[isopen=false]:hidden'
     >
-      <div className='bg-white rounded'>
+      <div ref={ref} className='bg-white rounded'>
         <button onClick={exit}>Exit</button>
       </div>
     </div>
