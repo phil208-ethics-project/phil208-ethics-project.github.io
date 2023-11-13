ff627e778700eb78b4dd67c63e939a83a280e699
Max Dillon <maxdillon13124@gmail.com>
2023-11-13 03:20:47 -0500

    feat: add basic style to student page

--- src/features/ManageStudent/pages/StudentPage.tsx
+++ src/features/ManageStudent/pages/StudentPage.tsx
@@ -26,11 +26,19 @@ export default function StudentPage() {
 
   return (
     <>
-      <p>{student?.first_name}</p>
-      <p>{student?.last_name}</p>
-      <button onClick={() => setOpenInformational(true)}>
-        Open InformationalGradesDialog
+      <h1 className='m-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
+        {student?.first_name}{' '}
+        <mark className='px-2 text-white bg-slate-400 rounded '>
+          {student?.last_name}
+        </mark>
+      </h1>
+      <button
+        className='border-2 m-4 rounded p-2 hover:bg-gray-100 transition-colors text-xs text-gray-700 uppercase font-bold'
+        onClick={() => setOpenInformational(true)}
+      >
+        Grade Student!
       </button>
+
       <InformationalGradesDialog
         isOpen={openInformational}
         exit={() => setOpenInformational(false)}
