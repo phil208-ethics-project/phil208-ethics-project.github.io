80adf6ea81dc8de90e7879623c3199f107cbfdac
Sean McGuckin <smcgucki@purdue.edu>
2023-12-03 20:25:42 -0500

    Generalizing file consumption

--- src/features/SaveData/components/FileUpload.tsx
+++ src/features/SaveData/components/FileUpload.tsx
@@ -1,33 +1,22 @@
 import { useRef, useState } from 'react'
 
-import { db } from '@db'
-import { parse } from "papaparse";
+interface FileUploadProps {
+  onUpload: (files: FileList) => void | Promise<void>
+}
 
-// interface FileUploadProps {
-//   onUpload: (files: FileList) => void | Promise<void>
-// }
-
-// export default function FileUpload({ onUpload }: FileUploadProps) {
-export default function FileUpload() {
+export default function FileUpload({ onUpload }: FileUploadProps) {
   const fileUploadElement = useRef<HTMLInputElement>(null)
   const [over, setOver] = useState(false)
   return (
     <div
       data-over={over}
-      className='data-[over=true]:p-64 data-[over=true]:border-4 border-dotted inline-block'
+      className='inline-block'
       onDrop={e => {
         e.preventDefault()
         e.stopPropagation()
         setOver(false)
-        // onUpload(e.dataTransfer.files)
-        Array.from(e.dataTransfer.files)
-            .filter((file) => file.type === "text/csv")
-            .forEach(async (file) => {
-              const text = await file.text();
-              const result = parse(text, { header: true });
-              console.log(result) // print statement basically
-              db.students.bulkAdd(result);
-            });
+        onUpload(e.dataTransfer.files)
+
       }}
       onDragOver={e => {
         e.preventDefault()
@@ -48,7 +37,7 @@ export default function FileUpload() {
         multiple
         onChange={e => {
           const target = e.target as HTMLInputElement & { files: FileList }
-          // onUpload(target.files)
+          onUpload(target.files)
         }}
       />
       <button
@@ -56,7 +45,7 @@ export default function FileUpload() {
         onClick={() => fileUploadElement.current?.click()}
       >
         <div className="flex items-center justify-center w-full">
-          <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+          <label htmlFor="dropzone-file" data-over={over} className="data-[over=true]:bg-gray-100 flex flex-col items-center justify-center h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
             <div className="flex flex-col items-center justify-center pt-5 pb-6 pr-6 pl-6">
               <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
--- src/features/StudentDashboard/pages/StudentPage.tsx
+++ src/features/StudentDashboard/pages/StudentPage.tsx
@@ -2,8 +2,44 @@ import NewStudentForm from '../components/NewStudentForm'
 import StudentTable from '../components/StudentTable'
 import FileUpload from '@features/SaveData/components/FileUpload'
 
+import { Student, db } from '@db'
+import { parse } from "papaparse";
+
+
 import FuzzyStudentSearch from '@components/FuzzyStudentSearch'
 import useSetTitle from '@hooks/useSetTitle'
+import { readZip } from '@features/SaveData/lib/readFile';
+
+
+
+async function onUpload(files: FileList) {
+  let fileText: { [name: string]: string}= {}
+  const analyzeFiles = Array.from(files).map(async file => {
+    if (file.name.endsWith(".zip")) {
+      const zipFileResult = await readZip(file)
+      fileText = {...fileText, ...zipFileResult}
+    } else {
+      fileText[file.name] = await file.text()
+    }
+
+  })
+
+  await Promise.all(analyzeFiles)
+
+  for (let [filename, text] of Object.entries(fileText)) {
+    try {
+      const parsed = parse<unknown>(text)
+      console.log(filename, parsed.data)
+      const students: Student[] = parsed.data.map(arr => ({first_name: arr[0], last_name: arr[1], age: arr[2], gender: arr[3]}))
+      db.students.bulkAdd(students)
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+
+}
+
 
 export default function StudentDastboard() {
   useSetTitle('Students | PHIL 208 Ethics Project')
@@ -21,7 +57,7 @@ export default function StudentDastboard() {
         <div className='mx-0 sm:mx-6 flex sm:flex-row gap-6 sm:gap-0 space-x-10'>
           <div className='w-1/4'>
             <h1 className='mb-4'>Upload Students</h1>
-            <FileUpload />
+            <FileUpload onUpload={onUpload} />
           </div>
           <div className='w-1/4'>
             <h1 className='mb-4'>Enter Students</h1>
