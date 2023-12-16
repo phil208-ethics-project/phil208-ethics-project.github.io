16db690eda142f10911427ed50e9693c7f6336e9
Sean McGuckin <smcgucki@purdue.edu>
2023-12-16 12:53:35 -0500

    Enforced zod schemas

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,66 +1,135 @@
 import Dexie, { Table } from 'dexie'
 import { z } from 'zod'
 
-export interface Session {
-  id?: number
-  date: number
-  name?: string
-}
+// export interface Session {
+//   id?: number
+//   date: number
+//   name?: string
+// }
+
+export const sessionSchema = z.object({
+  id: z.number().preprocess(Number, z.number()).optional(),
+  date: z.number().preprocess(Number, z.number()),
+  name: z.string().optional()
+})
+
+export type Session = z.infer<typeof sessionSchema>
+
+// export interface Student {
+//   id?: number
+//   first_name: string
+//   last_name: string
+//   gender: 'male' | 'female' | 'nonbinary' | 'other'
+//   age: number
+// }
 
 export const studentSchema = z.object({
-  id: z.number().optional(),
+  id: z.number().preprocess(Number, z.number()).optional(),
   first_name: z.string(),
   last_name: z.string(),
-  age: z.number(),
+  age: z.number().preprocess(Number, z.number()),
   gender: z.enum(['male', 'female', 'nonbinary', 'other']),
 })
 
 export type Student = z.infer<typeof studentSchema>
 
-export interface FictionalGrade {
-  session_id: number
-  student_id: number
-  v: boolean
-  kd: boolean
-  ca: boolean
-  i: boolean
-  e: boolean
-  l: boolean
-  go: boolean
-  mi: boolean
-}
+// export interface FictionalGrade {
+//   session_id: number
+//   student_id: number
+//   v: boolean
+//   kd: boolean
+//   ca: boolean
+//   i: boolean
+//   e: boolean
+//   l: boolean
+//   go: boolean
+//   mi: boolean
+// }
 
-export interface InformationalGrade {
-  session_id: number
-  student_id: number
-  v: boolean
-  kd: boolean
-  ar: boolean
-  i: boolean
-  e: boolean
-  l: boolean
-  tf: boolean
-  mi: boolean
-}
+export const fictionalGradeSchema = z.object({
+  session_id: z.number().preprocess(Number, z.number()),
+  student_id: z.number().preprocess(Number, z.number()),
+  v: z.boolean().preprocess(Boolean, z.boolean()),
+  kd: z.boolean().preprocess(Boolean, z.boolean()),
+  ca: z.boolean().preprocess(Boolean, z.boolean()),
+  i: z.boolean().preprocess(Boolean, z.boolean()),
+  e: z.boolean().preprocess(Boolean, z.boolean()),
+  l: z.boolean().preprocess(Boolean, z.boolean()),
+  go: z.boolean().preprocess(Boolean, z.boolean()),
+  mi: z.boolean().preprocess(Boolean, z.boolean())
+})
 
-export interface SpellingGrade {
-  session_id: number
-  student_id: number
-  phonetic_short_vowels: string
-  phonetic_consonant_blends: string
-  phonetic_consonant_digraphs: string
-  transitional_long_vowels: string
-  transitional_complex_vowels: string
-  fluent_inflectional_endings: string
-  fluent_multisyllabic_words_2_syllabes: string
-  advanced_multisyllabic_words_3_syllabes: string
-}
+export type FictionalGrade = z.infer<typeof fictionalGradeSchema>
 
-export interface ReadingLevelGrade {
-  session_id: number
-  student_id: number
-  reading_level: number
-}
+// export interface InformationalGrade {
+//   session_id: number
+//   student_id: number
+//   v: boolean
+//   kd: boolean
+//   ar: boolean
+//   i: boolean
+//   e: boolean
+//   l: boolean
+//   tf: boolean
+//   mi: boolean
+// }
+
+export const informationalGradeSchema = z.object({
+  session_id: z.number().preprocess(Number, z.number()),
+  student_id: z.number().preprocess(Number, z.number()),
+  v: z.boolean().preprocess(Boolean, z.boolean()),
+  kd: z.boolean().preprocess(Boolean, z.boolean()),
+  ar: z.boolean().preprocess(Boolean, z.boolean()),
+  i: z.boolean().preprocess(Boolean, z.boolean()),
+  e: z.boolean().preprocess(Boolean, z.boolean()),
+  l: z.boolean().preprocess(Boolean, z.boolean()),
+  tf: z.boolean().preprocess(Boolean, z.boolean()),
+  mi: z.boolean().preprocess(Boolean, z.boolean())
+})
+
+export type InformationalGrade = z.infer<typeof informationalGradeSchema>
+
+// export interface SpellingGrade {
+//   session_id: number
+//   student_id: number
+//   phonetic_short_vowels: string
+//   phonetic_consonant_blends: string
+//   phonetic_consonant_digraphs: string
+//   transitional_long_vowels: string
+//   transitional_complex_vowels: string
+//   fluent_inflectional_endings: string
+//   fluent_multisyllabic_words_2_syllabes: string
+//   advanced_multisyllabic_words_3_syllabes: string
+// }
+
+export const spellingGradeSchema = z.object({
+  session_id: z.number().preprocess(Number, z.number()),
+  student_id: z.number().preprocess(Number, z.number()),
+  phonetic_short_vowels: z.string(),
+  phonetic_consonant_blends: z.string(),
+  phonetic_consonant_digraphs: z.string(),
+  transitional_long_vowels: z.string(),
+  transitional_complex_vowels: z.string(),
+  fluent_inflectional_endings: z.string(),
+  fluent_multisyllabic_words_2_syllabes: z.string(),
+  advanced_multisyllabic_words_3_syllabes: z.string()
+})
+
+export type SpellingGrade = z.infer<typeof spellingGradeSchema>
+
+// export interface ReadingLevelGrade {
+//   session_id: number
+//   student_id: number
+//   reading_level: number
+// }
+
+export const readingLevelSchema = z.object({
+  session_id: z.number().preprocess(Number, z.number()),
+  student_id: z.number().preprocess(Number, z.number()),
+  reading_level: z.number().preprocess(Number, z.number())
+})
+
+export type ReadingLevelGrade = z.infer<typeof readingLevelSchema>
 
 export class StudentGradesDB extends Dexie {
   sessions!: Table<Session, number>
